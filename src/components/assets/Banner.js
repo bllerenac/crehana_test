ae222cf99b0ff56f449fafeeee4eeb4a15effddb
bllerenac <bllerenac@uni.pe>
2021-02-25 15:03:37 -0500

    banner component complete, add Banner .js and .css

---
+++ src/components/assets/Banner.js
@@ -0,0 +1,11 @@
+
+function Banner() {
+  return(
+    <section className="banner-container">
+      <h2>Title H2-<br/>banner cursos nuevos</h2>
+      <button className="button primary medium">Comprar Ahora</button>
+    </section>
+  );
+}
+
+export default Banner;
